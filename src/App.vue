0dce9826b54f84d578cd1694da5b8c2d900b8bd7
programador <rodriguesluizhenrique316@gmail.com>
2025-01-28 18:07:10 -0300

    Conhecendo a estrutura VueJs

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-for="(obj, index) in todos" v-bind:key="obj.id">
+    <h1> {{ index }} - {{ obj.name }}</h1>
+  </div> 
+  <img alt="Vue logo" src="./assets/logo.png">
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      todos: [
+          {
+            "id": 1,
+            "name": "Alice",
+            "data": "2025-01-28"
+          },
+          {
+            "id": 2,
+            "name": "Bob",
+            "data": "2025-01-28"
+          },
+          {
+            "id": 3,
+            "name": "Carlos",
+            "data": "2025-01-28"
+          },
+          {
+            "id": 4,
+            "name": "Diana",
+            "data": "2025-01-28"
+          },
+          {
+            "id": 5,
+            "name": "Evelyn",
+            "data": "2025-01-28"
+          }
+      ]
+  }
+}
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>Vue.js News</h1>
+        <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        </nav>
+    </header>
+</template>
+<script>
+
+export default {
+    
+}
+
+</script>
+
+<style scoped>
+header {
+    background-color: #bdbdbd;
+    color: white;
+    padding: 10px 0;
+    text-align: center;
+}
+nav ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+
+}
+</style>
