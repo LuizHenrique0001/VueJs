2effab0913da0452c3aeaebd72606fc8e8175d31
programador <rodriguesluizhenrique316@gmail.com>
2025-01-31 15:14:29 -0300

    Conhecendo modificadores de evento

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,19 @@
 <template>
   <div>
-    <div>
-      one way binding<br>
-      Two way binding<br>
-      v-model formularios
-    </div>
-    <br>
-    <br>
-    <div>
-      <label for="">Nome</label><br>
-      <input type="text" v-model="name">
-      <br>
-      {{ name }}
-    </div>
-    <br><br>
-    <div>
-      <label for="">Carro</label><br>
-      <select v-model="carr">
-        <option value="">Escolha</option>
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
-      </select>
-      <br>
-      {{ carr }}
-    </div>
-    <br><br>
-    <div>
-      <label for="">Noticia</label>
-      <input type="radio" value="sim" v-model="noticia">Sim
-      <input type="radio" value="nao" v-model="noticia">Nao
-      <br><br>
-      {{ noticia }}
-    </div>
-    <div>
-      <label for="">Contrato</label><br>
-      <input type="checkbox" value="sim" v-model="contrato">Aceita Nosso...
-      <br><br>
-      {{ contrato }}
-    </div>
-    <br><br>
-    <div>
-      <label for="">Cores</label><br>
-      <input type="checkbox" value="Amarelo" v-model="cores">Amarelo
-      <input type="checkbox" value="Vermelho" v-model="cores">Vermelho
-      <input type="checkbox" value="Azul" v-model="cores">Azul
-      <br><br>
-      {{ cores }}
-    </div>
+     <button @click.once="onclick">Enviar</button>
+     <h2></h2>
   </div> 
+  <br><br>
+  <div>
+    <button @mousemove="mover">opa</button>
+  </div>
+  <br><br>
+  <form action="https://google.com" @submit.prevent="onSubmit">
+    <input type="text"  @keyup="onKeyup" id="">
+    <button type="submit">
+      Enviar
+    </button>
+  </form>
 </template>
 
 <script>
@@ -57,11 +21,21 @@ export default {
   name: 'App',
   data(){
     return {
-      name: 'Luiz Henrique',
-      carr: '',
-      noticia: '',
-      contrato: false,
-      cores: []
+     
+    }
+  },
+  methods: {
+    onclick($evt){
+      console.log('clicou', $evt)
+    },
+    mover($evt){
+      console.log('Estas moviendo el mouse', $evt)
+    },
+    onSubmit(){
+      console.log('Formulario enviado')
+    },
+    onKeyup($evt){
+      console.log('Estas escribiendo', $evt)
     }
   }
 }
